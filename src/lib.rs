f4c4b0a0a7d41e19542a91201ac3aeb429c20fbb
Dimitri Aatos Ellinas <d.a.ellinas@gmail.com>
2025-07-04 01:35:18 +0300

    feat: pitch bend sensitivity setter method

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@ pub enum Mode {
     Mode5,
 }
 
+#[derive(Clone, Copy)]
 pub enum Zone {
     Lower,
     Upper,
@@ -93,9 +94,9 @@ impl MIDIChannel {
             },
         }
     }
-    // pub fn pitch_bend_sensitivity(&mut self, sensitivity: u8) {
-    //     self.pitch_bend_sensitivity = sensitivity
-    // }
+    pub fn pitch_bend_sensitivity(&self) -> u8 {
+        self.pitch_bend_sensitivity
+    }
 }
 
 #[derive(Clone)]
@@ -218,19 +219,30 @@ impl MPEState {
             _ => {}
         };
     }
-    pub fn member_channels(&self, zone: &Zone) -> Option<&[Channel]> {
+    pub fn active(&self) -> bool {
+        matches!(self.channels.first().unwrap(), Channel::Manager { .. })
+            || matches!(self.channels.last().unwrap(), Channel::Manager { .. })
+    }
+
+    // Zone methods
+    pub fn zone_member_channel_range(&self, zone: &Zone) -> Option<Range<usize>> {
         match self.channels[zone.manager_index()] {
             Channel::Manager {
                 member_channels, ..
             } => {
                 let manager_offset = 1;
-                Some(self.zone_slice(
-                    zone,
-                    manager_offset..(member_channels as usize + manager_offset),
-                ))
+                Some(manager_offset..(member_channels as usize + manager_offset))
             }
             _ => None,
         }
+    }
+    pub fn zone_member_channels(&self, zone: &Zone) -> Option<&[Channel]> {
+        self.zone_member_channel_range(zone)
+            .map_or(None, |range| Some(&self.channels[range]))
+    }
+    pub fn zone_member_channels_mut(&mut self, zone: &Zone) -> Option<&mut [Channel]> {
+        self.zone_member_channel_range(zone)
+            .map_or(None, |range| Some(&mut self.channels[range]))
     }
     pub fn zone_channel_range(&self, zone: &Zone) -> Option<Range<usize>> {
         match self.channels[zone.manager_index()] {
@@ -247,26 +259,12 @@ impl MPEState {
         }
     }
     pub fn zone_channels(&self, zone: &Zone) -> Option<&[Channel]> {
-        match self.channels[zone.manager_index()] {
-            Channel::Manager {
-                member_channels, ..
-            } => {
-                let manager_offset = 1;
-                Some(self.zone_slice(zone, 0..(member_channels as usize + manager_offset)))
-            }
-            _ => None,
-        }
+        self.zone_channel_range(zone)
+            .map_or(None, |range| Some(&self.channels[range]))
     }
     pub fn zone_channels_mut(&mut self, zone: &Zone) -> Option<&mut [Channel]> {
-        match self.channels[zone.manager_index()] {
-            Channel::Manager {
-                member_channels, ..
-            } => {
-                let manager_offset = 1;
-                Some(self.zone_slice_mut(zone, 0..(member_channels as usize + manager_offset)))
-            }
-            _ => None,
-        }
+        self.zone_channel_range(zone)
+            .map_or(None, |range| Some(&mut self.channels[range]))
     }
     fn compute_range(zone: &Zone, range: Range<usize>) -> Range<usize> {
         let manager_index = zone.manager_index();
@@ -278,8 +276,36 @@ impl MPEState {
     pub fn zone_slice_mut(&mut self, zone: &Zone, range: Range<usize>) -> &mut [Channel] {
         &mut self.channels[Self::compute_range(zone, range)]
     }
-    pub fn active(&self) -> bool {
-        matches!(self.channels.first().unwrap(), Channel::Manager { .. })
-            || matches!(self.channels.last().unwrap(), Channel::Manager { .. })
+
+    // channel methods
+    fn zone_by_channel(&self, channel: &u8) -> Option<Zone> {
+        let index = (channel - 1) as usize;
+        [Zone::Lower, Zone::Upper]
+            .iter()
+            .find(|z| {
+                self.zone_channel_range(&z)
+                    .map_or(false, |r| r.contains(&index))
+            })
+            .copied()
+    }
+    pub fn set_pitch_bend_sensitivity(&mut self, channel: u8, pitch_bend_sensitivity: u8) {
+        let zone = self.zone_by_channel(&channel).unwrap();
+        match &mut self.channels[channel as usize - 1] {
+            Channel::Manager { channel, .. } | Channel::Conventional { channel } => {
+                channel.pitch_bend_sensitivity = pitch_bend_sensitivity;
+            }
+            Channel::Member { .. } => {
+                // changing a single member channel's pitch bend sensitivity
+                // should be reflected to all member channels of the zone
+                self.zone_member_channels_mut(&zone)
+                    .unwrap()
+                    .iter_mut()
+                    .for_each(|channel| {
+                        if let Channel::Member { channel } = channel {
+                            channel.pitch_bend_sensitivity = pitch_bend_sensitivity;
+                        }
+                    });
+            }
+        }
     }
 }
